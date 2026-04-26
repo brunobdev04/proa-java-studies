5265ff826936fd308f3d68c74a7db664035c54c2
Bruno B. Silva <contato.brunobsilva@outlook.com>
2026-04-26 14:15:06 -0300

    create navbar

---
+++ Instagraos/instagram-clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return ( 
+        <nav style={{ borderBottom: '1px solid #dbdbdb', padding: '12px 24px' }}>
+            <h1 style={{ fontFamily: 'cursive', fontSize: '26px' }}>Instagram</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
